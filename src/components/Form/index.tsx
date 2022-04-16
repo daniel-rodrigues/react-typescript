6c8078f892fcda59ef5da1909c1eebdedcc97a72
Daniel Rodrigues <inf.daniel@gmail.com>
2022-04-16 01:00:09 -0300

    Create Form component

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,34 @@
+﻿import React from "react";
+import Button from "../Button";
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form>
+        <div>
+          <label htmlFor="task">Add a new study</label>
+          <input
+            type="text"
+            id="task"
+            name="task"
+            placeholder="What do you want to study?"
+          />
+        </div>
+        <div>
+          <label htmlFor="time">Time</label>
+          <input
+            type="time"
+            id="time"
+            step="1"
+            min="00:00:00"
+            max="01:30:00"
+            required
+          />
+        </div>
+        <Button />
+      </form>
+    );
+  }
+}
+
+export default Form;
