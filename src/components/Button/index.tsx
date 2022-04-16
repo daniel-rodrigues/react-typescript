cc7d91d8fbb11cda3dd637dc01eb040e7c53b9c8
Daniel Rodrigues <inf.daniel@gmail.com>
2022-04-16 00:59:56 -0300

    Create button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button>
+        button
+      </button>
+    );
+  }
+}
+
+export default Button;
